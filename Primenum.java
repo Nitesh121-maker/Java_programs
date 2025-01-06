e1a893addaa39b5824d49261b584d7751a610e4e
Nitesh Chauhan <72297535+Nitesh121-maker@users.noreply.github.com>
2025-01-06 16:24:41 +0530

    NS_COMMIT 16:24 06-01-2025

---
+++ Primenum.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Primenum {
+    static  boolean isPrime(int n){
+        if (n<=1) {
+            return false;
+        }
+        
+        // Check for prime
+        for(int i = 2; i<n; i++){
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the num");
+        n = scanner.nextInt();
+        if(isPrime(n)){
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
+        }
+        scanner.close();
+    }
+}
